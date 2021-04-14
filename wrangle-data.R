da45b4498ff1c903b19e840934644260998f34ad
hegde-dev <hegde.shankar@gmail.com>
2021-04-14 18:48:41 +0530

    Adding new files

---
+++ wrangle-data.R
@@ -0,0 +1,8 @@
+library(magrittr)
+library(dplyr)
+library(tidyvers)
+library(dslabs)
+
+murders <- read.csv("data/murders.csv")
+murders <- murders %>% mutate(region = factor(region),rate = total / population * 10^5)
+save(murders,file = "rda/murders.rda")
